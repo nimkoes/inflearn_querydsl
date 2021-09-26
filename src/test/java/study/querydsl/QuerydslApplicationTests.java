76503db0f95423f8152df7eacefb9d7c5746a154
nimkoes <xxxelppa@gmail.com>
2021-09-26 21:19:51 +0900

    스터디에서 사용 할 샘플 엔티티 기본 설정 및 확인용 테스트 코드

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,16 +1,17 @@
 package study.querydsl;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.HelloEntity;
+import study.querydsl.entity.Member;
 import study.querydsl.entity.QHelloEntity;
-import static org.assertj.core.api.Assertions.*;
+import study.querydsl.entity.Team;
 
 @SpringBootTest
 @Transactional
@@ -32,6 +33,34 @@ class QuerydslApplicationTests {
         assertThat(result).isEqualTo(helloEntity);
         assertThat(result.getId()).isEqualTo(helloEntity.getId());
 
+    }
+
+    @Test
+    public void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println(" -> member.team = " + member.getTeam());
+        }
     }
 
 }
